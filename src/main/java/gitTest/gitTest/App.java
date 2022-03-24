478249c5d361d380184e04adead18da79258b3fd
kim <an2p92@naver.com>
2022-03-24 10:54:48 +0900

    ddd

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -12,10 +12,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
         System.out.println( "Hello tttttttt!" );
-=======
         System.out.println( "Hello aaaaa" );
->>>>>>> testBranch
     }
 }

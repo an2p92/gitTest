b48e5d181bd3dd0b46f8e596258393b1289911ce
seul <lspin117@gmail.com>
2022-03-25 13:00:39 +0900

    king받넴

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -11,5 +11,6 @@ public class App2
         System.out.println( "Hello World!" );
         System.out.println( "Hello gggggggWorld!" );
         System.out.println( "oooooooooooooppppppp!" );
+        System.out.println( "Hello World!" );
     }
 }

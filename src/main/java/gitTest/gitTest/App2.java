103e4a52582355cd0f938a761f4b775a949fe0b0
ji <khouse0924@gmail.com>
2022-04-20 13:18:15 +0900

    ddddd

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -8,40 +8,8 @@ public class App2
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        
-        System.out.println( "git test!" );
-        System.out.println( "야호!" );
-        System.out.println( "0ㅂ0!" );
-        
-        System.out.println( "으아아아아아앗!" );
-        System.out.println( "d-2일" );
-        System.out.println( "0ㅂ0으아아아아아악 무섭드아!!!!!!!!!!!!!" );
-        
-        System.out.println( "비가오는날엔~~~~~~~!" );
-        System.out.println( "우산 안가져옴 ;; 아  지갑도 안가져왔다 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ" );
-        
-        System.out.println( "전 대빵큰 장우산도있구요" );
-        System.out.println( "지갑도 들고왔는데 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ" );
-        System.out.println( "비오는날엔 피전에 막걸리!!꾸에에에에!!!!!!!!!!!!!!" );
-        
-        
-        System.out.println( "Hello gggggggWorld!" );
-        System.out.println( "oooooooooooooppppppp!" );
-        
         System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
         System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        
         System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
         System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
         System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");

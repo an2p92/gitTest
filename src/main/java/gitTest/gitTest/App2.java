b228cd09f84b59d35efdb11167af780ee8caf572
ji <khouse0924@gmail.com>
2022-04-20 13:25:54 +0900

    aaaa

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -15,15 +15,6 @@ public class App2
         System.out.println( "야호!" );
         System.out.println( "0ㅂ0!" );
         
-        System.out.println( "으아아아아아앗!" );
-        System.out.println( "d-2일" );
-        System.out.println( "d-2일" );
-        System.out.println( "d-2일" );
-        System.out.println( "d-2일" );
-        System.out.println( "d-2일" );
-        System.out.println( "0ㅂ0으아아아아아악 무섭드아!!!!!!!!!!!!!" );
-        
-        System.out.println( "비가오는날엔~~~~~~~!" );
         System.out.println( "우산 안가져옴 ;; 아  지갑도 안가져왔다 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ" );
         
         System.out.println( "전 대빵큰 장우산도있구요" );

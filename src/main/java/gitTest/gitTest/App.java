86ab32eafb0a3e9aabbd355a9b78298276cd09fe
ji <khouse0924@gmail.com>
2022-04-20 13:28:32 +0900

    뭐머 추가

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -23,19 +23,6 @@ public class App
         System.out.println( "bbbbbbbb" );
         System.out.println( "cccccccc" );
         
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "꺄아아아아아아아아아아아아" );
-        System.out.println( "bbbbbbbb" );
-        System.out.println( "cccccccc" );
-        System.out.println( "Aㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ" );
-        System.out.println( "BㅏBㅗ" );
         
         System.out.println( "으아아아아아아아아아아" );
         System.out.println( "D-4" );

aef6aedc3238903c663996cb53b28f4a32fe8e13
dongmin <ehdals0318@naver.com>
2022-04-20 15:15:36 +0900

    졸립니다

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -10,14 +10,24 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-        System.out.println( "aaaaa" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello tttttttt!" );
-        System.out.println( "Hello tttttttt!" );
-        System.out.println( "Hello vvvvvvv" );
-        System.out.println( "qqqqqq" );
-        System.out.println( "qqqqqq" );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        System.out.println( "그만하시오 이제 " );
+        
         
         System.out.println( "aaaaaaaa" );
         System.out.println( "bbbbbbbb" );

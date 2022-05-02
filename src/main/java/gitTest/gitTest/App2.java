08d888cfba0e1cd31efa4c05f93a3816f300976d
lindsayseo <lindsayshw87@gmail.com>
2022-05-02 14:49:48 +0900

    clean

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -9,38 +9,6 @@ public class App2
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        
-        System.out.println( "test1" );
-        System.out.println( "test2" );
-        System.out.println( "test3" );
-        System.out.println( "test4" );
-        
-        System.out.println( "test by shin" );
-        System.out.println( "test by shin" );
-        System.out.println( "test by wook" );
-        System.out.println( "test by wook" );
-        
-        System.out.println( "수정한부분입니다~~~~~~~~~~" );
-        
-        System.out.println( "git test!" );
-        System.out.println( "야호!" );
-        System.out.println( "0ㅂ0!" );
-        
-        System.out.println( "안녕하세요 먼저 테스트!");
-        
-        System.out.println( "우산 안가져옴 ;; 아  지갑도 안가져왔다 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ" );
-        
-        System.out.println( "전 대빵큰 장우산도있구요" );
-        System.out.println( "지갑도 들고왔는데 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ.ㅋ" );
-        System.out.println( "비오는날엔 피전에 막걸리!!꾸에에에에!!!!!!!!!!!!!!" );
-        
-        
-        System.out.println( "Hello gggggggWorld!" );
-        System.out.println( "oooooooooooooppppppp!" );
-        
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~월요일~~~~~~~~~");
-        System.out.println("와우~~~~~~~~~~~~~~~~~~~수요일이다ㅏㅏㅏㅏㅏㅏㅏㅏ");
-        
+                
     }
 }

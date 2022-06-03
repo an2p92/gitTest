bc304860b706d805845fc7f0f677f37746f8cb6b
mjkim <mjkim0378@naver.com>
2022-06-03 13:27:33 +0900

    hello2

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -10,5 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "2022-05-02ddldlldldldldl" );
+        System.out.println( "Hello Hello Hello Hello" );
+        System.out.println( "Hello2 Hello2 Hello2 Hello2" );
+        
     }
 }

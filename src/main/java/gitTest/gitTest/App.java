34c169f21710fcadfa2078388bf7a4c72e4fc099
mjkim <mjkim0378@naver.com>
2022-06-03 12:59:12 +0900

    aaaa

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -10,22 +10,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "2022-05-02ddldlldldldldl" );
-        System.out.println( "bbbbbbbb" );
-        System.out.println( "cccccccc" );
-        System.out.println( "으아아아아아아아아아아" );
-        System.out.println( "D-4" );
-        System.out.println( "qqqqqq" );
-        System.out.println( "wwwwww" );
-        System.out.println( "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 테테테ㅔ테테테테ㅔ테" );
-        System.out.println( "wwwwww" );
-        System.out.println( "wwwwww" );
-        System.out.println( "wwwwww" );
-        System.out.println( "wwwwww" );
-        System.out.println( "wwwwww" );
-        System.out.println( "금요일.....넘흐 무섭다" );
-        System.out.println( "우아아아아아 d-3" );
-        System.out.println( "나는.........과연.................... " );
-        System.out.println( "나는.........과연.................... " );
-        System.out.println( "나는.........과연.................... " );
     }
 }
--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -9,22 +9,5 @@ public class App2
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("dkdkdkkdkdk여기는 테스트브런치ㅣㅣㅣㅣㅣ");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test222222222");
     }
 }

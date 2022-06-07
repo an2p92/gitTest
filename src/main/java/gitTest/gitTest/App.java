2e175ed8358a0d791412128fb92bb323c9c74564
your <jks_gg@naver.com>
2022-06-07 09:35:23 +0900

    test

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println( "Hello2 Hello2 Hello2 Hello2" );
         System.out.println( "test1" );
         System.out.println( "안녕하세요 test~" );
+        System.out.println( "test3" );
     }
 }

9ddf613d52c3edc4cb75c03d1c94d3f768e2dca1
ROHHANSEO <sola4441@gmail.com>
2022-08-03 13:53:29 +0900

    as

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -14,6 +14,14 @@ public class App2
         System.out.println( "Hello3 Hello3 Hello3 Hello3" );
         System.out.println( "test1" );
         System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
+        System.out.println( "test2" );
         System.out.println( "안녕하세요 test~" );
         System.out.println( "오늘은 금요일" );
         System.out.println( "오늘은 금요일" );

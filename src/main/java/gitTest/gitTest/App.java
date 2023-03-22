a6abcd01b5fa123719c182f0864db425f40bed6f
k30ehs6 <k30ehs6@gmail.com>
2023-03-22 13:18:10 +0900

    dddd

--- src/main/java/gitTest/gitTest/App.java
+++ src/main/java/gitTest/gitTest/App.java
@@ -12,5 +12,6 @@ public class App
     	System.out.println("branch 테스트");
     	System.out.println("merge 테스트");
     	System.out.println("merge 테스트");
+    	System.out.println("여기는 aa브런치 입니다");
     }
 }

8e63673a05d54ea540ee446439e7bf7d9995d5f3
k30ehs6 <k30ehs6@gmail.com>
2023-03-22 13:07:14 +0900

    깃 테스트

--- src/main/java/gitTest/gitTest/App2.java
+++ src/main/java/gitTest/gitTest/App2.java
@@ -12,28 +12,5 @@ public class App2
     	System.out.println("branch 테스트");
     	System.out.println("merge 테스트");
     	System.out.println("merge 테스트");
-    	
-    	System.out.println( "Hello World!" );
-        System.out.println( "aaaaa" );
-        System.out.println( "Hello2 Hello2 Hello2 Hello2" );
-        System.out.println( "Hello3 Hello3 Hello3 Hello3" );
-        System.out.println( "test1" );
-        System.out.println( "test2" );
-        System.out.println( "안녕하세요 test~" );
-        System.out.println( "오늘은 금요일" ); 
-        System.out.println( "오늘은 금요일" );
-        System.out.println( "오늘은 금요일" );
-        System.out.println( "오늘은 금요일" );
-        System.out.println( "오늘은 금요일" );
-        System.out.println( "오늘은 dddddddddddddddddddddddddddddddd 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "오늘은 금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일오늘은 금요일~~" );
-        System.out.println( "가나다라마바사아자차카타파하" );
-        System.out.println( "System.out.println( \"3시퇴근\");" );
-        System.out.println("test");
     }
 }
